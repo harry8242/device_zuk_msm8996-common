1c5b925d5d4aad5dda0ba1c1d4f3de1502642071
Michael Wright <michaelwr@google.com>
2017-01-31 18:34:15 +0000

    zuk:Custom Vibrator implementation
    Add support for new amplitude controls.
    
    Test: Manual testing with vendor/google_experimental/users/michaelwr/Vibrator
          & adb shell /data/nativetest/vibrator_hidl_hal_test/vibrator_hidl_hal_test
    Change-Id: I7e8c489169d2955d3c5fcc7e6aa0e9aa12bf88a2

--- hidl.mk
+++ hidl.mk
@@ -92,7 +92,7 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.0-impl
+    android.hardware.vibrator@1.0-service.msm8996
 
 # WiFi HAL
 PRODUCT_PACKAGES += \
